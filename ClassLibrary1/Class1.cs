d94321620a1f7640adbb8e26e6703158a0bd5743
Silver530 <62809725+Silver530@users.noreply.github.com>
2022-06-25 02:42:10 +0400

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculation
+    {
+        public static int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            int result = 0;
+            double material=0, product=0;
+            double multiply;
+
+            if (productType < -1 || productType > 3 ||
+                materialType < -1 || materialType > 2 ||
+                count < 0 || length < 0 || width < 0)
+            {
+                return -1;
+            }
+            else {
+                switch (productType)
+                {
+                    case 0: product = 1.1; break;
+                    case 1: product = 2.5; break;
+                    case 2: product = 8.43; break;
+                }
+                switch (materialType)
+                {
+                    case 0: material = 0.3; break;
+                    case 1: material = 0.12; break;
+                }
+
+                multiply = width * length * count * product;
+                result = Convert.ToInt32(multiply + Math.Ceiling(material / 100));
+                return result;
+            }
+
+               // anektod
+               // 
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int actual = Calculation.GetQuantityForProduct(1, 1, 1, 1, -4);
+            Assert.AreEqual(-1, actual);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int actual = Calculation.GetQuantityForProduct(3, 1, 15, 20, 45);
+            Assert.AreEqual(114148, actual);
+
+        }
+    }
+}
